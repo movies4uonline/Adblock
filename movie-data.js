bae756068b1ff00a8adbe2047afaab85d6cca7e3
movies4uonline <moviesu537@gmail.com>
2025-07-29 02:11:38 +0530

    Update movie-data.js

--- movie-data.js
+++ movie-data.js
@@ -1,8 +1,8 @@
 
 const movies = {
   trending: [
-    { title: "Movie 1", poster: "assets/posters/movie1.jpg", video: "https://example.com/video1" },
-    { title: "Movie 2", poster: "assets/posters/movie2.jpg", video: "https://example.com/video2" },
+    { title: "Movie 1", poster: "https://i.ibb.co/9HH39xpt/images-90.jpg", video: "https://stream.movies4uu.workers.dev/dl/68846b8be288ad3ceaeea169" },
+    { title: "Movie 2", poster: "https://i.ibb.co/4RqKtcms/images-91.jpg", video: "https://example.com/video2" },
     { title: "Movie 3", poster: "assets/posters/movie3.jpg", video: "https://example.com/video3" },
     { title: "Movie 4", poster: "assets/posters/movie4.jpg", video: "https://example.com/video4" },
     { title: "Movie 5", poster: "assets/posters/movie5.jpg", video: "https://example.com/video5" }
